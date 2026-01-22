b4ebd4e285d5b04056469bdf15bdb9a5a266f0a6
zakkycrypt01 <zakkycrypt@gmail.com>
2026-01-22 21:42:37 +0100

    chore: add lib/i18n/use-i18n.ts

---
+++ lib/i18n/use-i18n.ts
@@ -0,0 +1,9 @@
+/**
+ * useI18n Hook Export
+ * Re-exports from i18n-context for convenience
+ */
+
+'use client';
+
+// Re-export from i18n-context
+export { useI18n, useLanguage, useTranslation } from './i18n-context';
